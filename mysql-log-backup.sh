a5d3d510fe05531b41b2a2d34c6fd05a518f4883
James McDonald <james@toggen.com.au>
2020-01-30 11:27:29 +1100

    version 1 of script

---
+++ mysql-log-backup.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+LOG_DIR=$HOME/mysql-log-backup/ln09-logs
+
+LOGIN_PATH=logShipper
+
+MYSQL_HOST=192.168.0.15
+
+LAST_LOG=$(ls -1 $LOG_DIR/mysql-bin.* | tail -n 1)
+
+LAST_LOG=$(basename $LAST_LOG)
+
+mysqlbinlog --login-path=$LOGIN_PATH --read-from-remote-server \
+    --host=$MYSQL_HOST \
+    --raw --stop-never \
+    $LAST_LOG
